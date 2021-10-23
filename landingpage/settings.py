3095ab004d84bc095d492df18f410d5c8dc06a91
Cedrouse Roll <54283441+zedjarvis@users.noreply.github.com>
2021-10-23 12:16:46 +0300

    Update settings.py
    changed staticfiles_storage middleware

--- landingpage/settings.py
+++ landingpage/settings.py
@@ -166,7 +166,7 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, "static"),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
