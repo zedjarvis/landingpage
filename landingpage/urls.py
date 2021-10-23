daad90840874862028365caa99668835310d974e
Cedrouse Roll <54283441+zedjarvis@users.noreply.github.com>
2021-10-23 12:00:30 +0300

    Update urls.py
    included path to myprofile urls.py

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from MyProfile import views as myprofile_views
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', myprofile_views.index, name='index')
+     path('', include('MyProfile.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
