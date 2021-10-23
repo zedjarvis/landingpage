da17100fe6a47e7bac68d50426197b8603541e6e
Cedrouse Roll <54283441+zedjarvis@users.noreply.github.com>
2021-10-23 12:41:21 +0300

    Update settings.py
    ADDED MEDIA UPLOAD DIRS

--- landingpage/settings.py
+++ landingpage/settings.py
@@ -162,6 +162,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
